4f682cb55f8944679fe9ce7601b2f7b740082452
Ramandeep Singh <42717721+rzing@users.noreply.github.com>
2019-04-11 19:26:29 +0530

    Updated configuration properties

--- website/js/config.js
+++ website/js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ABCDEFG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1a2b3c4d5f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-west-2_voeWMzuTT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '20n17sg4qa0ammajcf7v7gv7bf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://abcdefghijkl.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://gefekr2sml.execute-api.us-west-2.amazonaws.com/Prod' 
+        // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'earl@eeg3.net'
+        email: 'Ramandeep.Singh@capgemini.com'
     }
 };
